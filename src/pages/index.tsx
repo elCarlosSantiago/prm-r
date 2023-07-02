daeedad8e84ca7b88897e5daf52e944b6bac13f1
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-02 15:20:36 -0700

    signed in button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
 import Head from "next/head";
-import Link from "next/link";
-import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  const user = useUser()
   return (
     <>
       <Head>
@@ -13,6 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div>
+          {user.isSignedIn ? <SignOutButton/> : <SignInButton/>}
+        </div>
       </main>
     </>
   );
