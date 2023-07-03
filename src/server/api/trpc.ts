890074525ee4766b670c2d399560cce66adcba01
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 16:43:48 -0700

    updated trpc context

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,18 +1,10 @@
-/**
- * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1).
- * 2. You want to create a new middleware or type of procedure (see Part 3).
- *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
+/*
+ * Modifiy Context
+ * Add new middlewares or procedure types
  */
 
-/**
+/*
  * 1. CONTEXT
- *
- * This section defines the "contexts" that are available in the backend API.
- *
- * These allow you to access things when processing a request, like the database, the session, etc.
  */
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next"
 
@@ -27,9 +19,7 @@ import {
 import { ZodError } from "zod"
 
 /**
- * This is the actual context you will use in your router. It will be used to process every request
- * that goes through your tRPC endpoint.
- *
+ * This will process every request that goes through the endpoint.
  * @see https://trpc.io/docs/context
  */
 
@@ -44,16 +34,13 @@ const createInnerTRPCContext = ({ auth }: AuthContext) => {
   }
 }
 
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
-  //AUTH PATH IS BROKEN - getAuth is not working
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
   return createInnerTRPCContext({ auth: getAuth(opts.req) })
 }
 export type Context = inferAsyncReturnType<typeof createTRPCContext>
 
 /**
  * 2. INITIALIZATION
- *
- * This is where the tRPC API is initialized, connecting the context and transformer.
  */
 
 const t = initTRPC.context<Context>().create({
@@ -71,29 +58,18 @@ const t = initTRPC.context<Context>().create({
 })
 
 /**
- * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
- *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
- * "/src/server/api/routers" directory.
+ * 3. ROUTER & PROCEDURE
  */
 
 /**
- * This is how you create new routers and sub-routers in your tRPC API.
- *
+ * How to create new routers & sub routers
  * @see https://trpc.io/docs/router
  */
 export const createTRPCRouter = t.router
 
-/**
- * Public (unauthenticated) procedure
- *
- * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
- * guarantee that a user querying is authorized, but you can still access user session data if they
- * are logged in.
- */
 export const publicProcedure = t.procedure
 
-const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+const userIsAuthenticated = t.middleware(({ ctx, next }) => {
   if (!ctx.auth.userId) {
     throw new TRPCError({
       code: "UNAUTHORIZED",
@@ -107,4 +83,4 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
   })
 })
 
-export const privateProcedure = t.procedure.use(enforceUserIsAuthed)
+export const privateProcedure = t.procedure.use(userIsAuthenticated)
