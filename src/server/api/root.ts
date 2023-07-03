7b8c00c28e57e025fa7b12818240a0e84712f6f3
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-02 17:32:26 -0700

    products router added to app router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { exampleRouter } from "~/server/api/routers/example";
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter } from '~/server/api/trpc'
+import { productsRouter } from './routers/products.router'
 
 /**
  * This is the primary router for your server.
@@ -7,8 +7,8 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
-});
+  products: productsRouter,
+})
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
