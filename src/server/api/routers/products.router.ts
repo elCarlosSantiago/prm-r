57d00a2bc2c079afbe44d673f0637b1ae1b5a82d
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 13:05:09 -0700

    product router CRUD up

--- src/server/api/routers/products.router.ts
+++ src/server/api/routers/products.router.ts
@@ -1,13 +1,46 @@
-import { z } from "zod"
-
-import {
-  createTRPCRouter,
-  privateProcedure,
-  publicProcedure,
-} from "~/server/api/trpc"
+import { createTRPCRouter, privateProcedure } from "~/server/api/trpc"
+import { productSchema } from "~/schemas/product.schema"
+import { notFound } from "~/utils/errors"
+import { idSchema } from "~/schemas/id.schema"
 
 export const productsRouter = createTRPCRouter({
   getAll: privateProcedure.query(({ ctx }) => {
     return ctx.prisma.product.findMany()
+  }),
+
+  getById: privateProcedure.input(idSchema).query(async ({ ctx, input }) => {
+    const product = await ctx.prisma.product.findUnique({
+      where: { id: input.id },
+    })
+    if (!product) throw notFound()
+    return product
+  }),
+
+  create: privateProcedure
+    .input(productSchema)
+    .mutation(async ({ ctx, input }) => {
+      const product = await ctx.prisma.product.create({
+        data: input,
+      })
+      return product
+    }),
+
+  edit: privateProcedure
+    .input(productSchema)
+    .mutation(async ({ ctx, input }) => {
+      const product = await ctx.prisma.product.update({
+        where: { id: input.id },
+        data: input,
+      })
+      if (!product) throw notFound()
+      return product
+    }),
+
+  delete: privateProcedure.input(idSchema).mutation(async ({ ctx, input }) => {
+    const product = await ctx.prisma.product.delete({
+      where: { id: input.id },
+    })
+    if (!product) throw notFound()
+    return product
   }),
 })
