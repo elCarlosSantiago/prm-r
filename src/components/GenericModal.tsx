a2f9466791f4211b42d554dbb0617fef710e25b9
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 18:55:04 -0700

    generic modal width edited

--- src/components/GenericModal.tsx
+++ src/components/GenericModal.tsx
@@ -12,7 +12,7 @@ export const GenericModal: React.FC<{
       aria-hidden="true"
       className="z-50 flex h-full max-h-full w-full overflow-y-auto overflow-x-hidden border border-red-500 p-4"
     >
-      <div className="m-auto max-h-full w-full max-w-2xl border border-red-400">
+      <div className="m-auto max-h-full w-full max-w-3xl">
         <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
           <div className="flex items-start justify-between rounded-t border-b p-4 dark:border-gray-600">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
