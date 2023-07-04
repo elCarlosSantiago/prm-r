e531e93f44f160e5f8f2e5bd09d4eafb0b2f0b75
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 21:57:14 -0700

    input updated to forwardRef component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,6 @@
+import { forwardRef } from "react"
+import { type UseFormRegisterReturn } from "react-hook-form"
+
 type InputProps = {
   size?: "sm" | "md" | "lg"
   label?: string
@@ -8,82 +11,74 @@ type InputProps = {
   error?: string
   disabled?: boolean
   step?: number
+  register?: UseFormRegisterReturn //TODO: Fix this
 }
 
-export const Input: React.FC<InputProps> = ({
-  size = "md",
-  label,
-  placeholder,
-  value,
-  onChange,
-  type = "text",
-  error,
-  disabled,
-  step,
-}) => {
-  if (size === "sm")
-    return (
-      <div>
-        {label && (
-          <label
-            htmlFor="small-input"
-            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            {label}
-          </label>
-        )}
-        <input
-          type={type}
-          id="small-input"
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs"
-          placeholder={placeholder}
-          min={0}
-          step={step ?? 0.01}
-        />
-      </div>
-    )
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ size = "md", label, ...inputProps }, ref) => {
+    if (size === "sm")
+      return (
+        <div>
+          {label && (
+            <label
+              htmlFor="small-input"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+            >
+              {label}
+            </label>
+          )}
+          <input
+            id="small-input"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs"
+            min={0}
+            {...inputProps}
+            ref={ref}
+          />
+        </div>
+      )
 
-  if (size === "md") {
+    if (size === "md") {
+      return (
+        <div className="mb-6">
+          {label && (
+            <label
+              htmlFor="base-input"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+            >
+              {label}
+            </label>
+          )}
+          <input
+            min={0}
+            ref={ref}
+            {...inputProps}
+            id="base-input"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          />
+        </div>
+      )
+    }
+
     return (
       <div className="mb-6">
         {label && (
           <label
-            htmlFor="base-input"
+            htmlFor="large-input"
             className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
           >
             {label}
           </label>
         )}
         <input
-          type={type}
           min={0}
-          step={step ?? 0.01}
-          id="base-input"
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          placeholder={placeholder}
+          ref={ref}
+          {...inputProps}
+          id="large-input"
+          className="sm:text-md block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
         />
       </div>
     )
   }
+)
 
-  return (
-    <div className="mb-6">
-      {label && (
-        <label
-          htmlFor="large-input"
-          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-        >
-          {label}
-        </label>
-      )}
-      <input
-        type={type}
-        min={0}
-        step={step ?? 0.01}
-        id="large-input"
-        placeholder={placeholder}
-        className="sm:text-md block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-      />
-    </div>
-  )
-}
+Input.displayName = "Input"
