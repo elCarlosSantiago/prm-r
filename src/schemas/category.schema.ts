c5c6879747193ed988cc4e148da2f153f9efba89
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 22:52:18 -0700

    category schema updated

--- src/schemas/category.schema.ts
+++ src/schemas/category.schema.ts
@@ -4,7 +4,7 @@ import { productInputSchema } from "./product.schema"
 export const categorySchema = z.object({
   id: z.string(),
   name: z.string(),
-  products: productInputSchema.array(),
+  products: productInputSchema.array().optional(),
 })
 
 export type Category = z.infer<typeof categorySchema>
