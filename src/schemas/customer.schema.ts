52b3ab9d2e09205a04368ee755bddc7c208773be
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-04 14:59:16 -0700

    customer schema updated

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -1,13 +1,30 @@
 import { z } from "zod"
-import { orderInputSchema } from "./order.schema"
-
+import { orderItemSchema } from "~/schemas"
 export const customerSchema = z.object({
   id: z.string().optional(),
   firstName: z.string(),
   lastName: z.string(),
   email: z.string(),
   phone: z.string(),
-  orders: orderInputSchema.array(),
+  orders: z
+    .object({
+      id: z.string(),
+      createdAt: z.date(),
+      status: z.string(),
+      total: z.number(),
+      orderItem: orderItemSchema,
+      trackingCompany: z.string(),
+      address: z.object({
+        id: z.string(),
+        line1: z.string(),
+        line2: z.string().optional(),
+        city: z.string(),
+        state: z.string(),
+        zip: z.string(),
+        country: z.string(),
+      }),
+    })
+    .array(),
 })
 
 export type Customer = z.infer<typeof customerSchema>
