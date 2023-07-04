c512bd8a829fede591a63f76219e1fabac08ae08
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 18:14:45 -0700

    GenericModal up

--- src/components/GenericModal.tsx
+++ src/components/GenericModal.tsx
@@ -1,7 +1,7 @@
 export const GenericModal: React.FC<{
   title: string
   submit: () => void
-  cancel: () => void
+  cancel: React.Dispatch<React.SetStateAction<boolean>>
   submitText?: string
   cancelText?: string
   children: React.ReactNode
@@ -10,9 +10,9 @@ export const GenericModal: React.FC<{
     <div
       id="defaultModal"
       aria-hidden="true"
-      className="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full overflow-y-auto overflow-x-hidden p-4 md:inset-0"
+      className="z-50 flex h-full max-h-full w-full overflow-y-auto overflow-x-hidden border border-red-500 p-4"
     >
-      <div className="relative max-h-full w-full max-w-2xl">
+      <div className="m-auto max-h-full w-full max-w-2xl border border-red-400">
         <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
           <div className="flex items-start justify-between rounded-t border-b p-4 dark:border-gray-600">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
@@ -20,8 +20,13 @@ export const GenericModal: React.FC<{
             </h3>
             <button
               type="button"
-              className="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="ml-auto inline-flex  items-center rounded-lg border bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="defaultModal"
+              onClick={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                cancel(false)
+              }}
             >
               <svg
                 aria-hidden="true"
@@ -31,9 +36,9 @@ export const GenericModal: React.FC<{
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               <span className="sr-only">Close modal</span>
@@ -53,7 +58,11 @@ export const GenericModal: React.FC<{
               data-modal-hide="defaultModal"
               type="button"
               className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-600"
-              onClick={cancel}
+              onClick={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                cancel(false)
+              }}
             >
               {cancelText || "Cancel"}
             </button>
