a359c916db646d234258d68af9e01a74fd99059d
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 18:56:31 -0700

    added queries to product page

--- src/templates/ProductPage.tsx
+++ src/templates/ProductPage.tsx
@@ -1,17 +1,35 @@
 import { api } from "~/utils"
 import { LoadingPage, ProductModal, ProductTable } from "~/components"
 import React, { useState } from "react"
-import { type RouterOutputs } from "~/utils/api"
-
-type Product = RouterOutputs["products"]["getAll"][number]
+import { type ProductExport } from "~/schemas"
 
 export const ProductPage: React.FC = () => {
-  const { data: products, isLoading } = api.products.getAll.useQuery()
+  //State & Context
   const [openProductModal, setOpenProductModal] = useState<boolean>(false)
-  const [selectedProduct, setSelectedProduct] = useState<Product | undefined>(
-    undefined
-  )
-  console.log({ openProductModal, selectedProduct })
+  const [selectedProduct, setSelectedProduct] = useState<
+    ProductExport | undefined
+  >(undefined)
+  const ctx = api.useContext()
+  //Query
+  const { data: products, isLoading } = api.products.getAll.useQuery()
+  const { mutate: editProduct } = api.products.edit.useMutation({
+    onSuccess: () => {
+      void ctx.products.getAll.invalidate()
+      setSelectedProduct(undefined)
+    },
+    onError: (err) => {
+      const errorMessage = err.data?.zodError?.fieldErrors.content
+      if (errorMessage && errorMessage[0]) {
+        alert(errorMessage[0])
+      } else {
+        alert("Failed to edit! Please try again later.")
+      }
+    },
+  })
+
+  const handleSubmit = (product?: ProductExport) => {
+    if (product) editProduct(product)
+  }
   if (isLoading) return <LoadingPage />
   return (
     <React.Fragment>
@@ -19,6 +37,7 @@ export const ProductPage: React.FC = () => {
         <ProductModal
           product={selectedProduct}
           close={() => setOpenProductModal(false)}
+          submit={handleSubmit}
         />
       )}
       <div className="gap-12 p-12">
