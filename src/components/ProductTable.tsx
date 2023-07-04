41c60ebe07d11dea144746812adfd4d70bc2d009
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-03 17:26:09 -0700

    products table up

---
+++ src/components/ProductTable.tsx
@@ -0,0 +1,85 @@
+import { type RouterOutputs } from "~/utils/api"
+
+type Products = RouterOutputs["products"]["getAll"]
+export const ProductTable = ({
+  products,
+}: {
+  products: Products | undefined
+}) => {
+  if (!products) return null
+  return (
+    <div className="mx-auto max-w-5xl">
+      <div className="flex flex-col">
+        <div className="overflow-x-auto shadow-md sm:rounded-lg">
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden ">
+              <table className="min-w-full table-fixed divide-y divide-gray-200 dark:divide-gray-700">
+                <thead className="bg-gray-100 dark:bg-gray-700">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400"
+                    >
+                      Product Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400"
+                    >
+                      Category
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400"
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400"
+                    >
+                      Stock
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-800">
+                  {products?.map((product) => (
+                    <tr
+                      className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                      key={product.id}
+                    >
+                      {/* <td className="w-4 p-4">
+                        <div className="flex items-center justify-center">
+                          <input
+                            id="checkbox-table-1"
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                          />
+                          <label htmlFor="checkbox-table-1" className="sr-only">
+                            checkbox
+                          </label>
+                        </div>
+                      </td> */}
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900 dark:text-white">
+                        {product.name}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-500 dark:text-white">
+                        {product.category.name}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900 dark:text-white">
+                        ${product.price}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900 dark:text-white">
+                        {product.stock}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/templates/ProductPage.tsx
+++ src/templates/ProductPage.tsx
@@ -1,16 +1,13 @@
 import { api } from "~/utils"
 import { LoadingPage } from "~/components"
+import { ProductTable } from "~/components/ProductTable"
 
 export const ProductPage: React.FC = () => {
   const { data: products, isLoading } = api.products.getAll.useQuery()
   if (isLoading) return <LoadingPage />
   return (
-    <div>
-      {products?.map((product) => (
-        <div key={product.id} className="border-b border-slate-400 p-8">
-          {product.name}
-        </div>
-      ))}
+    <div className="gap-12 p-12">
+      <ProductTable products={products} />
     </div>
   )
 }
