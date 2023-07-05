296db138ebe40d7da70dcd60f3f8d28b4f07036b
Carlos Santiago Nevett <csnevett@gmail.com>
2023-07-05 10:25:31 -0700

    category schema up

--- src/schemas/category.schema.ts
+++ src/schemas/category.schema.ts
@@ -2,9 +2,16 @@ import { z } from "zod"
 import { productInputSchema } from "./product.schema"
 
 export const categorySchema = z.object({
-  id: z.string(),
+  id: z.string().optional(),
   name: z.string(),
   products: productInputSchema.array().optional(),
 })
 
+export const categoryInputSchema = categorySchema.omit({
+  id: true,
+  products: true,
+})
+
 export type Category = z.infer<typeof categorySchema>
+
+export type CategoryInput = z.infer<typeof categoryInputSchema>
